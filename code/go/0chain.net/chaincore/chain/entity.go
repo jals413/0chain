d6cc42b734c798f0a8bfcf507b0aabbc9c20ed64
peterlimg <peterlimg@protonmail.com>
2024-12-28 21:20:38 +1100

    Log calling stack for set magic block

--- code/go/0chain.net/chaincore/chain/entity.go
+++ code/go/0chain.net/chaincore/chain/entity.go
@@ -1019,7 +1019,7 @@ func (c *Chain) SetMagicBlock(mb *block.MagicBlock) {
 		logging.Logger.Error("failed to put magic block", zap.Error(err))
 	}
 
-	logging.Logger.Debug("[mvc] set magic block",
+	logging.Logger.Error("[mvc] set magic block",
 		zap.Int64("mb number", mb.MagicBlockNumber),
 		zap.Int64("mb sr", mb.StartingRound),
 		zap.String("mb hash", mb.Hash))
