467451f708ca3b03116de951baf27775cf0fbdfb
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-12-31 23:43:35 +0530

    Fix rewards event tag key

--- code/go/0chain.net/smartcontract/dbs/event/challenge_pool.go
+++ code/go/0chain.net/smartcontract/dbs/event/challenge_pool.go
@@ -22,13 +22,35 @@ func (edb *EventDb) addOrUpdateChallengePools(cps []ChallengePool) error {
 	}).Create(&cps).Error
 }
 
-func mergeAddChallengePoolsEvents() *eventsMergerImpl[ChallengePool] {
-	return newEventsMerger[ChallengePool](TagAddOrUpdateChallengePool, withUniqueEventOverwrite())
-}
-
 func (edb *EventDb) GetChallengePool(allocationID string) (*ChallengePool, error) {
 	var cp ChallengePool
 	return &cp, edb.Store.Get().Model(&ChallengePool{}).
 		Where("allocation_id = ?", allocationID).
 		Take(&cp).Error
+}
+
+func mergeAddChallengePoolsEvents() *eventsMergerImpl[ChallengePool] {
+	return newEventsMerger[ChallengePool](TagAddOrUpdateChallengePool, withUniqueEventOverwrite())
+}
+
+func mergeToChallengePoolsEvents() *eventsMergerImpl[ChallengePool] {
+	return newEventsMerger[ChallengePool](TagToChallengePool, withMergeToChallengePool())
+}
+
+func mergeFromChallengePoolsEvents() *eventsMergerImpl[ChallengePool] {
+	return newEventsMerger[ChallengePool](TagFromChallengePool, withMergeFromChallengePool())
+}
+
+func withMergeToChallengePool() eventMergeMiddleware {
+	return withEventMerge(func(a, b *ChallengePool) (*ChallengePool, error) {
+		a.Balance += b.Balance
+		return a, nil
+	})
+}
+
+func withMergeFromChallengePool() eventMergeMiddleware {
+	return withEventMerge(func(a, b *ChallengePool) (*ChallengePool, error) {
+		a.Balance -= b.Balance
+		return a, nil
+	})
 }
--- code/go/0chain.net/smartcontract/dbs/event/process.go
+++ code/go/0chain.net/smartcontract/dbs/event/process.go
@@ -215,6 +215,8 @@ func mergeEvents(round int64, block string, events []Event) ([]Event, error) {
 
 			mergeUpdateChallengesEvents(),
 			mergeAddChallengePoolsEvents(),
+			mergeToChallengePoolsEvents(),
+			mergeFromChallengePoolsEvents(),
 
 			mergeUpdateBlobberChallengesEvents(),
 			mergeUpdateAllocChallengesEvents(),
